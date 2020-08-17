eaeafc87ff2e10832fd33ce388f4b513dcf6f3e6
AndreiSuprun <66207171+AndreiSuprun@users.noreply.github.com>
2020-08-17 03:04:26 +0300

    Delete AppliancePredicate.java

--- main/java/org/suprun/filesearch/model/predicate/AppliancePredicate.java
+++
@@ -1,9 +0,0 @@
-package org.suprun.filesearch.model.predicate;
-
-import java.util.function.BiPredicate;
-import java.util.function.Predicate;
-
-public class AppliancePredicate {
-
-    public BiPredicate<String, String> predicate = (String f, String par) -> f.contains(par);
-}
